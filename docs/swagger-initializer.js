779af49dc882b0cbc5782f614b74e261982dcb90
BillChai <Chaiyomin@gmailcom>
2024-10-16 14:59:24 +0900

    fix swagger path

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     // url: "https://petstore.swagger.io/v2/swagger.json",
-    url: "../swagger.yaml", // my swagger file
+    url: "./swagger.yaml", // my swagger file
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
